fa307f980ae058ac17e4310d56596d33c1b2ff75
ODanielCosta <32438454+ODanielCosta@users.noreply.github.com>
2022-10-28 18:28:57 +0100

    Display funciona, inicio de local storage

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 let arrayItemList = [];
+arrayItemList[0] = "";
+let taskList = document.getElementById("taskList");
 
+
+
+//Função do botão
 function getTaskInput ()
 {
+    //Vai buscar valor do input e mete em "x"
     let x = document.getElementById("taskInput").value;
+    // Coloca o que estava no input dentro da array
     arrayItemList.push(x);
 
+
+    localStorage.setItem("ALISTA", arrayItemList);
+
+    // Executa o print na tela
     for (let i = 0; i < arrayItemList.length; i++)
     {
-        list = arrayItemList[i] + "<br>";
+        taskList.innerHTML = arrayItemList.join("<li>") + "</li>" + "<br>";
     }
-    document.getElementById("taskList").innerHTML = list;
+
 }
+
+
+
+
 
