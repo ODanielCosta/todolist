a1484a544f919a1480176d168103c407b6e18507
ODanielCosta <32438454+ODanielCosta@users.noreply.github.com>
2022-12-09 15:25:52 +0000

    Login inicio

--- login.php
+++ login.php
@@ -1,17 +1,18 @@
 <?php
-    include_once 'header.php';
+include_once 'header.php';
 ?>
-    <body>
-        <main>
-            <h3>Login</h3>
-            <form action="login.inc.php" method="post">
-                <input type="text" name="name" placeholder="Username or Email...">
-                <input type="password" name="pwd" placeholder="Password...">
-                <button type="submit" name="submit">Login</button>
-        </main>
-    <body
-    
 
-<?php
-    include_once 'footer.php';
-?>
+<body>
+    <main>
+        <h3>Login</h3>
+        <form action="includes/login.inc.php" method="post">
+            <input type="text" name="name" placeholder="Username or Email...">
+            <input type="password" name="pwd" placeholder="Password...">
+            <button type="submit" name="submit">Login</button>
+    </main>
+
+    <body>
+
+        <?php
+        include_once 'footer.php';
+        ?>
